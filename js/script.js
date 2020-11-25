2b29daea5d415874d04160a49af68b7b9dc2d582
Nuria Home <n.m.mustafina@gmail.com>
2020-11-25 16:32:28 +0600

    Task 012

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-"use strict";
+// "use strict";
 
-const category = 'toys';
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
 
-console.log(`https://someurl.com/${category}/5`);
+// const okCancel = confirm("Ok or Cancel?");
+// console.log(okCancel);
+
+
+//Задание 012
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+// console.log(numberOfFilms);
+
+// const personalMovieDB = {
+//     count:numberOfFilms,
+//     movies:"",
+//     actors:"",
+//     genres:[],
+//     privat:false,
+// };
+// console.log(personalMovieDB);
+
+const movie = prompt("Один из последних просмотренных фильмов?","");
+const movieRating = prompt("На сколько оцените его?","");
+const movies = [movie, movieRating];
+console.log(movies);
+
+// personalMovieDB[0] = numberOfFilms;
+// personalMovieDB[1] = prompt("gfvhj?","");
